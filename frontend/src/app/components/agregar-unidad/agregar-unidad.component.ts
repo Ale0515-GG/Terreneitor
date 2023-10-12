b437c34c10f8a7be1a5d6434c1d068ed19df3e77
Luisgatovolador <116209151+Luisgatovolador@users.noreply.github.com>
2023-10-12 11:32:48 -0600

    v1.4.1

--- frontend/src/app/components/agregar-unidad/agregar-unidad.component.ts
+++ frontend/src/app/components/agregar-unidad/agregar-unidad.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UnidadService } from 'src/app/services/unidad/unidad.service';
+import { Router } from '@angular/router';
+import { Unidad } from 'src/app/interfaces/unidad';
 
 @Component({
   selector: 'app-agregar-unidad',
   templateUrl: './agregar-unidad.component.html',
   styleUrls: ['./agregar-unidad.component.css']
 })
-export class AgregarUnidadComponent {
+export class AgregarUnidadComponent implements OnInit {
+  unida: Unidad = {
+    NombrePropiedad: '',
+    Descripcion: '',
+    TipoPropiedad: '',
+    PrecioPorNoche: 0,
+    DireccionID: '',
+   
+  };
 
+  constructor(private unidadService: UnidadService, private router: Router) {}
+
+  ngOnInit(): void {}
+
+  saveNewUnidad() {
+    this.unidadService.saveUnidad(this.unida).subscribe(
+      (res) => {
+        console.log(res);
+        this.router.navigate(['/administar-unidad']);
+      },
+      (err) => console.error(err)
+    );
+  }
 }
