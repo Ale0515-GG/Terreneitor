7f2af91cf78ffaf16add0e54bf7b494b95208aca
Luisgatovolador <116209151+Luisgatovolador@users.noreply.github.com>
2023-10-19 16:03:14 -0600

    v 1.6

--- frontend/src/app/components/agregar-unidad/agregar-unidad.component.ts
+++ frontend/src/app/components/agregar-unidad/agregar-unidad.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UnidadService } from 'src/app/services/unidad/unidad.service';
 import { Router } from '@angular/router';
 import { Unidad } from 'src/app/interfaces/unidad';
+import { Storage, ref, uploadBytes, listAll, getDownloadURL, deleteObject } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-agregar-unidad',
@@ -17,10 +18,13 @@ export class AgregarUnidadComponent implements OnInit {
     DireccionID: '',
    
   };
+images:string[];
+  
+  constructor(private unidadService: UnidadService, private router: Router, private storage:Storage) {this.images=[];}
 
-  constructor(private unidadService: UnidadService, private router: Router) {}
-
-  ngOnInit(): void {}
+  ngOnInit(){
+    this.getImages
+  }
 
   saveNewUnidad() {
     this.unidadService.saveUnidad(this.unida).subscribe(
@@ -30,5 +34,31 @@ export class AgregarUnidadComponent implements OnInit {
       },
       (err) => console.error(err)
     );
+  }
+
+  uploadImages($event:any){
+    const file= $event.target.files[0];
+    console.log(file)
+    const imageRef= ref(this.storage,`images/${file.name}`);
+
+    uploadBytes(imageRef,file)
+    .then(response=> console.log(response))
+    .catch(error=> console.log(error))
+  }
+
+  getImages(){
+    const imagesRef= ref(this.storage,`imagenes`)
+    listAll(imagesRef)
+    .then(async response=>{
+      console.log(response)
+      this.images=[];
+      for(let item of response.items){
+       const url= await getDownloadURL(item);
+       this.images.push(url)
+      }
+    })
+    .catch(err=>{
+      console.log(err)
+    })
   }
 }
