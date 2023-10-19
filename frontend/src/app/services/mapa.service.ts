78f9751209af0926d65d4617acaa0e35a586598c
Ale0515-GG <116208731+Ale0515-GG@users.noreply.github.com>
2023-10-19 15:40:08 -0600

    v1.5.2

--- frontend/src/app/services/mapa.service.ts
+++ frontend/src/app/services/mapa.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Marker,Map } from 'mapbox-gl';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class MapaService {
 
+  private map?:Map;
+  private markers:Marker[]=[];
+
+  get isMapReady(){
+    return !!this.map;
+  }
+
   constructor() { }
+
+  setMap(map:Map){
+
+    this.map=map;
+
+    this.map.on('style.load',()=>{
+      
+    })
+  }
+
 }
