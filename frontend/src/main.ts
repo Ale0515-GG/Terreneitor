01f23ed3c77e2dd60fbd3c7a48db302b2afb16c5
Ale0515-GG <116208731+Ale0515-GG@users.noreply.github.com>
2023-10-19 11:46:43 -0600

    v1.6.0

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,6 +4,10 @@ import { AppModule } from './app/app.module';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 
+import Mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+ 
+Mapboxgl.accessToken = 'pk.eyJ1IjoiYWxlZ2ciLCJhIjoiY2xueGU5cjAxMGMzNTJqcGU2ZWl4cHJ3aSJ9.aco018tTQskT63Crif1T5Q';
+
 if(!navigator.geolocation){
   alert('El navegador no soporta la geolocalizacion')
   throw new Error('El navegador no soporta la geolocalizacion');
